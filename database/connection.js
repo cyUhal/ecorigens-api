f5466539e8145b3450a8012fd8776b72bc2b58ab
cyUhal <cyntiauhalde22@gmail.com>
2023-08-06 18:21:09 +0100

    configuracion de DB para apuntar para nuevo ip

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose')
 
 const connection = async() => {
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/ECORIGENS", {
+        await mongoose.connect("mongodb://mongo:3Se3pdWPbtqoeGECmECx@containers-us-west-78.railway.app:6013", {
             useNewUrlParser:true,
             useUnifiedTopology:true,
             useCreateIndex:true
